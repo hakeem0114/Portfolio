7f004921ec39e6697c09ed96d5153522746c7be3
hakeem <hakeem0114@gmail.com>
2023-02-22 00:53:26 -0500

    Styled the main section of page & used media queries to make it compatible for smartphones for now. Header is next

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -6,15 +6,7 @@ function Header() {
 
   return (
     <header className="header">
-          <nav className="nav">
-              <ul className='navList'>
-                  <a href=""><li>Home</li></a>
-                  <a href=""><li>About</li></a>
-                  <a href=""><li>Skills</li></a>
-                  <a href=""><li>Projects</li></a>
-                  <a href=""><li>Contact</li></a>
-              </ul>
-          </nav>
+          <h1>Header</h1>
     </header>
   )
 }
