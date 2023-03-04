7d5f9437c3f0f779313fdb6b129cf7e01d965277
hakeem <hakeem0114@gmail.com>
2023-03-04 17:17:57 -0500

    Fixed Typo on about

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -14,7 +14,7 @@ function About() {
         <h1>In Love With Technology & Designing</h1>
 
       <div className="about2">
-        <p> Hello There! Based in Ontario, Canada. I am an aspiring developer & mechatronics engineer. I find the immediate outcomes of applying technology to life's problems are fascinating and rewarding. The easy access to the wide breadth of programming knowledge without any expensive barries to entry is what first drew me to software development and wh I want to learn more everyday  .</p>
+        <p> Hello There! Based in Ontario, Canada. I am an aspiring developer & mechatronics engineer. I find the immediate outcomes of applying technology to life's problems are fascinating and rewarding. The easy access to the wide breadth of programming knowledge without any expensive barriers to entry is what first drew me to software development and why I want to learn more everyday  .</p>
         <button>
             <p>Resume</p>
             <a target="_blank" href="https://drive.google.com/file/d/1ifaT0pN0jmGtOVkP1MhgeNKONNgRUHZ7/view?usp=share_link">
